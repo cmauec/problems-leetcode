89da0fe03d27b91213a7eedba3659c10f374863f
Carlos Vaca <cmauricioec@gmail.com>
2023-01-27 23:14:55 -0500

    Add solution #4

--- two_sum.py
+++ two_sum.py
@@ -46,4 +46,12 @@ class Solution3:
                     return [i, j]
 
 
-print(Solution1().twoSum(list(range(10000000)), 56))
+class Solution4:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in nums and nums.index(complement) != i:
+                return [i, nums.index(complement)]
+
+
+print(Solution4().twoSum([2,7,11,15], 18))
